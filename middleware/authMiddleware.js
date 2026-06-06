d2334d44e6b3462335e0b4eecf0dc4ec1034dc4b
sahaymihir <mihir.sahay0123@hotmail.com>
2026-06-06 20:30:34 +0530

    feat: more ckeanup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,26 +3,26 @@ import jwt from 'jsonwebtoken';
 import blacklistTokenModel from '../src/models/blacklist.model.js';
 
 const authUser = expressAsyncHandler(async (req, res, next) => {
-    const token = req.cookies.jwt;
+	const token = req.cookies.jwt;
 
-    if (!token) {
-        res.status(401);
-        throw new Error('No token');
-    }
-    const checkBlacklist = await blacklistTokenModel.findOne({ token });
-    if (checkBlacklist) {
-        res.status(401);
-        throw new Error('Token not Valid');
-    }
+	if (!token) {
+		res.status(401);
+		throw new Error('No token');
+	}
+	const checkBlacklist = await blacklistTokenModel.findOne({ token });
+	if (checkBlacklist) {
+		res.status(401);
+		throw new Error('Token not Valid');
+	}
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.status(401);
-        throw new Error('Invalid token');
-    }
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch {
+		res.status(401);
+		throw new Error('Invalid token');
+	}
 });
 
 export default authUser;
